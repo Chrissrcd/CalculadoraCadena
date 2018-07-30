9b15ae20d6ecddfd3b8ff83754b7e4df87dc89b7
Chrissrcd <chris_rcd@hotmail.com>
2018-07-30 08:52:38 -0500

    Primera parte

--- CalculadoraCadena/src/CalculadoraCadena.java
+++ CalculadoraCadena/src/CalculadoraCadena.java
@@ -10,4 +10,27 @@
  */
 public class CalculadoraCadena {
     
+    public static void main(String[] args) {
+        
+        System.out.println(add("1,40,25,60"));
+        
+    }
+
+    public static int add(String numbers) {
+        int result = 0;
+        numbers+=',';
+        if (numbers.length() > 0) {
+            String n = "";
+            for (int i = 0; i < numbers.length(); i++) {
+                if (numbers.charAt(i) == ',') {
+                    result += Integer.parseInt(n);
+                    n = "";
+                } else {
+                    n += numbers.charAt(i);
+                }
+            }
+        }
+        return result;
+    }
+
 }
